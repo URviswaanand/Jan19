4cd8c4ff065456739003c22dc66290bdebd7b6c6
Student <Student@VMMB310-81>
2018-01-19 12:51:35 +0200

    The beginning start

---
+++ VBVISWA19jan/VBVISWA19jan/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VBVISWA19jan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //I can do it 
+            
+        }
+    }
+}
